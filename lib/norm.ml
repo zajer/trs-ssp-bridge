51889fc624ac561d85377b3ae37b14f5de4f63f9
zajer <pikus3@list.pl>
2021-03-14 01:59:42 +0100

    Added new functions for normalization of TTS with compliance to new version of Tracking_bigraph library.

--- lib/norm.ml
+++ lib/norm.ml
@@ -1,5 +1,13 @@
 open Common
+open Bigraph
+open Tracking_bigraph
 module IntMap = Map.Make(Int);;
+type states = (int, Big.t) Hashtbl.t
+
+let map_states sl =
+  let result = Hashtbl.create ~random:false (List.length sl) in
+  List.iteri (fun i s -> Hashtbl.add result i s.TTS.bigraph) sl; result
+
 (* Operations needed for normalizing a csv produced by the TRS library. *)
 let normalize_transition mapped_states (init_state_idx,res_state_idx,react_label,iso_init2lhs,iso_res2init,res_state)  =
   let new_res_state = IntMap.find res_state_idx mapped_states in
@@ -10,6 +18,26 @@ let normalize_transition mapped_states (init_state_idx,res_state_idx,react_label
       let iso_to_be_applied,_,_ = List.hd avail_isos in
       let new_iso_res2init = Bigraph.Iso.transform ~iso_dom:( Bigraph.Iso.inverse iso_to_be_applied) ~iso_codom:(Bigraph.Iso.codom iso_res2init |> make_id_iso) iso_res2init in
   (init_state_idx,res_state_idx,react_label,iso_init2lhs,new_iso_res2init,new_res_state)
+let normalize_single_transition mapped_states trans = 
+  let new_out_state = Hashtbl.find mapped_states trans.TTS.out_state_idx in
+  let iso_to_be_applied_on_actual_out_state = TBig.translate_equal ~from_b:new_out_state ~to_b:trans.actual_out_state  in
+    let new_iso_res2init = Fun.transform 
+      ~iso_dom:iso_to_be_applied_on_actual_out_state 
+      ~iso_codom:(Fun.to_list trans.residue |> List.map (fun (_,i2) -> i2 ) |> make_id_iso) 
+      trans.residue in
+  {
+    TTS.in_state_idx=trans.in_state_idx;
+    out_state_idx=trans.out_state_idx;
+    react_label=trans.react_label;
+    participants=trans.participants;
+    residue=new_iso_res2init;
+    actual_out_state=new_out_state
+  }
+let normalize_transitions mapped_states tl = 
+  List.map
+    (normalize_single_transition mapped_states)
+    tl
+
 let normalize_t t_parsed mapped_states = 
   List.map 
   (normalize_transition mapped_states)
@@ -32,6 +60,9 @@ let normalize_exported_ss s_parsed t_parsed orig_trans_file_name =
   let t_normalized_sll = t_normalized_2_sll t_normalized in
     Csv.save ((filename_without_extension "csv" orig_trans_file_name )^"_normalized.csv") (t_normalized_sll) ;
     t_normalized
+let normalize_exported_tts s_parsed t_parsed =
+  let states_mapped = map_states s_parsed in
+    normalize_transitions states_mapped t_parsed
 let normalize_exported_ss_csv ~states_file:sfn ~trans_file:tfn =
   let s = Csv.load sfn |> Parsing.parse_s
   and t = Csv.load tfn |> Parsing.parse_t_without_header in
--- tests/norm_tests.ml
+++ tests/norm_tests.ml
@@ -17,9 +17,27 @@ let norm_transition_1 _ =
   assert_equal nt_lhs2init lhs2init;
   assert_equal (Bigraph.Iso.apply nt_residue2init 2) (Some 3);
   assert_equal (Bigraph.Iso.apply nt_residue2init 1) (Some 2)
+let norm_transition_2 _ = 
+  let b0_template = "{(0, AT1:3),(1, A:0),(2, B:0),(3, AT1:3),(4, AT1:3),(5, AT2:1)}\n1 6 0\n100111\n011000\n000000\n000000\n000000\n000000\n000000\n({}, {ca1a2}, {(0, 1), (3, 1)})\n({}, {ca2a3}, {(3, 1), (4, 1)})\n({}, {ca2a4}, {(3, 1), (5, 1)})\n({}, {lc1a1}, {(0, 1)})\n({}, {lc1a3}, {(4, 1)})\n({}, {lc2a1}, {(0, 1)})\n({}, {lc2a3}, {(4, 1)})"
+  and b1_template = "{(0, AT1:3),(1, AT1:3),(2, A:0),(3, B:0),(4, AT1:3),(5, AT2:1)}\n1 6 0\n110011\n000100\n001000\n000000\n000000\n000000\n000000\n({}, {ca1a2}, {(0, 1), (1, 1)})\n({}, {ca2a3}, {(1, 1), (4, 1)})\n({}, {ca2a4}, {(1, 1), (5, 1)})\n({}, {lc1a1}, {(0, 1)})\n({}, {lc1a3}, {(4, 1)})\n({}, {lc2a1}, {(0, 1)})\n({}, {lc2a3}, {(4, 1)})"
+  and b1_iso = "{(0, AT1:3),(1, AT1:3),(2, A:0),(3, B:0),(4, AT1:3),(5, AT2:1)}\n1 6 0\n110011\n000000\n001100\n000000\n000000\n000000\n000000\n({}, {}, {(0, 1), (1, 1)})\n({}, {}, {(0, 1), (4, 1)})\n({}, {}, {(0, 1), (5, 1)})\n({}, {}, {(1, 1)})\n({}, {}, {(1, 1)})\n({}, {}, {(4, 1)})\n({}, {}, {(4, 1)})"
+  in
+  let lhs2init = Bigraph.Iso.of_list [(0, 1); (1, 2); (2, 0)]
+  and res2init = Bigraph.Fun.of_list [(0, 1); (1, 0); (2, 2); (3, 3); (4, 4); (5, 5)] in
+  let map_of_states = Hashtbl.create 10 
+  and transition_to_norm = { Tracking_bigraph.TTS.in_state_idx=1;out_state_idx=0;react_label="r1";participants=lhs2init;residue=res2init;actual_out_state=(Bigraph.Big.of_string b1_iso)}in
+  let _ = Hashtbl.add map_of_states 0 (Bigraph.Big.of_string b0_template) ; Hashtbl.add map_of_states 1 (Bigraph.Big.of_string b1_template) in
+  let normalized_transition = Norm.normalize_single_transition map_of_states transition_to_norm in
+  assert_equal normalized_transition.in_state_idx 1 ;
+  assert_equal normalized_transition.out_state_idx 0;
+  assert_equal normalized_transition.react_label "r1";
+  assert_equal normalized_transition.participants lhs2init;
+  assert_equal (Bigraph.Fun.apply normalized_transition.residue 2) (Some 3);
+  assert_equal (Bigraph.Fun.apply normalized_transition.residue 1) (Some 2)
 let suite =
   "Normalization" >::: [
     "Transition normalization test 1">:: norm_transition_1;
+    "Transition normalization test 2">:: norm_transition_1;
     
 ]
 
