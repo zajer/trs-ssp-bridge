8618c63324284032378396549619b7fb549fcdfc
zajer <pikus3@list.pl>
2021-03-17 18:58:46 +0100

    Added a unit test for parsing dest patterns.

--- tests/parsing_tests.ml
+++ tests/parsing_tests.ml
@@ -14,10 +14,26 @@ let test_parse_react_times_1 _ =
   let _ = Hashtbl.add expected_react_times "r1" 1;Hashtbl.add expected_react_times "r2" 3;Hashtbl.add expected_react_times "r3" 1
   in
   assert_equal ~msg:"Parsed react times are not equal to expected" expected_react_times parsed_react_times
+  
+let test_parse_patterns_1 _ =
+  let patterns_file = "patterns.csv"
+  and pattern_1_big = "{(0, AT2:1),(1, A:0),(2, B:0)}\n1 3 0\n100\n011\n000\n000\n({}, {x}, {(0, 1)})" |> Bigraph.Big.of_string
+  and pattern_2_big = "{(0, AT1:3),(1, AT1:3),(2, B:0),(3, A:0),(4, AT1:3),(5, AT2:1)}\n1 6 0\n110011\n000000\n000000\n000000\n000000\n000000\n001100\n({}, {ca1a2}, {(0, 1), (1, 1)})\n({}, {ca2a3}, {(1, 1), (4, 1)})\n({}, {ca2a4}, {(1, 1), (5, 1)})\n({}, {lc1a1}, {(0, 1)})\n({}, {lc1a3}, {(4, 1)})\n({}, {lc2a1}, {(0, 1)})\n({}, {lc2a3}, {(4, 1)})" |> Bigraph.Big.of_string in
+  let expected_pattern_1 = {Patterns.bigraph=pattern_1_big;description="yolo"}
+  and expected_pattern_2 = {Patterns.bigraph=pattern_2_big;description="swag"} in
+  let expected_patterns = [expected_pattern_1;expected_pattern_2]
+  and imported_patterns = Parsing.parse_destingation_patterns patterns_file in
+  assert_bool "Number of imported patterns differs from the number of expected patterns" (List.length expected_patterns = List.length imported_patterns);
+  assert_equal 
+    ~msg:"Imported patterns are not equal to expected"
+    expected_patterns
+    imported_patterns
+  
 let suite =
   "Parsing tests" >::: [
     "Parsing ctrls test 1">:: test_parse_ctrls_1;
-    "Parsing react times test 1">:: test_parse_react_times_1
+    "Parsing react times test 1">:: test_parse_react_times_1;
+    "Parsing destination state patterns test 1">:: test_parse_patterns_1
 ]
 
 let () =
