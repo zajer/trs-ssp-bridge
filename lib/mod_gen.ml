8dfe7d49cb9306400c8ef82f763ec8f1c998e41d
zajer <pikus3@list.pl>
2021-03-25 20:49:56 +0100

    Modified the source template and fixed template modification.

--- lib/mod_gen.ml
+++ lib/mod_gen.ml
@@ -25,27 +25,23 @@ let construct_module module_name number_of_agents =
   and module_types = fun num_a -> "\ttype i = "^i_type num_a^"\n\ttype t = Unreachable | Reachable of i*SetOfActions.t\n\ttype k = t list\n\ttype f = t -> int -> t" 
   and module_footer = fun () -> "end" in
   module_header module_name ^"\n\n"^ module_types number_of_agents ^ "\n\n"^ construct_set_of_actions_2_string () ^ "\n\n"^  construct_i_to_string number_of_agents ^"\n" ^ construct_k_to_string number_of_agents ^"\n"^ module_footer ();;
-let _read_file filename = 
-  let lines = ref [] in
-  let chan = open_in filename in
-  try
-    while true; do
-      lines := input_line chan :: !lines
-    done; !lines
-  with End_of_file ->
-    close_in chan;
-    List.rev !lines ;;
+
 let _replace_num_of_agents_in_template num_of_ags template = 
   let regex = Str.regexp "###NUM_OF_AGS###" in
   Str.replace_first regex (string_of_int num_of_ags) template
 let _replace_init_state_in_template init_state_str template = 
   let regex = Str.regexp "###INIT_STATE###" in
   Str.replace_first regex init_state_str template
+let _replace_num_of_states num_of_sts template =
+  let regex = Str.regexp "###NUM_OF_STS###" in
+  Str.replace_first regex (string_of_int num_of_sts) template
 let _generate_init_state_str num_of_ags = 
-  let part_result = List.init num_of_ags (fun i -> "("^(string_of_int i)^",0)") |> String.concat ";" in
+  let part_result = List.init num_of_ags (fun i -> "("^(string_of_int (i+1))^",0)") |> String.concat ";" in
   "[|"^part_result^"|]"
-let construct_module_content_based_on_template template number_of_agents =
+let construct_module_content_based_on_template template ~number_of_agents ~number_of_states =
   let init_state_str = _generate_init_state_str number_of_agents in
   _replace_init_state_in_template init_state_str template
   |>
   _replace_num_of_agents_in_template number_of_agents 
+  |>
+  _replace_num_of_states number_of_states
